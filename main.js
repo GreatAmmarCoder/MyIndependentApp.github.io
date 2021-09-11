f27e9e87420c137fb4e3b5e6e796f913c8bea8dc
GreatAmmarCoder <84329306+GreatAmmarCoder@users.noreply.github.com>
2021-09-11 22:03:51 +0530

    My independent app

---
+++ main.js
@@ -0,0 +1,20 @@
+//Minecraft
+function mc() {
+    window.location="mc.html"
+}
+//Free fire
+function ff() {
+    window.location="ff.html"
+}
+//Call of duty
+function cod() {
+    window.location="cod.html"
+}
+//Aspahlt 9
+function as() {
+    window.location="as.html"
+}
+//Back button
+function back() {
+    window.location="main.html"
+}
